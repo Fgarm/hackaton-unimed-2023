67915a3796d369430ad7adde60e7d6d3473a16d0
Fgarm <guilhermear.maturana@outlook.com>
2023-10-04 19:34:31 -0300

    Made funcionario and escala table

---
+++ BACKEND/funcionario/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+# Create your models here.
+
+class Funcionario(models.Model):
+    nome = models.CharField(max_length=255)
+    pontuacao = models.IntegerField()
+    Funcao = models.CharField(max_length=255)
+
+class Meta:
+        verbose_name = "Funcionario"
+        verbose_name_plural = "Funcionarios"
+
+        def __str__(self: Funcionario):
+            return self.nome
