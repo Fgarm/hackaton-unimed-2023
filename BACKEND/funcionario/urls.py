594bf2aa4dc9d494b1ddb50d12a3c22cbbeeaeba
eniira <catarinecruz@alunos.utfpr.edu.br>
2023-10-05 08:25:01 -0300

    fixing merge

--- BACKEND/funcionario/urls.py
+++ BACKEND/funcionario/urls.py
@@ -4,11 +4,6 @@ from .views import FuncionarioApiView
 #from .views import FuncionarioView
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('funcionario/<str:codigo', FuncionarioView.as_view({
-        'put': 'update',
-    }), name='funcionario'),
-=======
     
     path('criar-funcionario/', FuncionarioApiView.cadastrar_funcionario, name='criar-funcionario'),
     path('pegar-funcionario/', FuncionarioApiView.pegar_funcionario, name='pegar-funcionario'),
@@ -16,6 +11,5 @@ urlpatterns = [
     #path('funcionario/<str:codigo', FuncionarioView.as_view({
     #    'put': 'update',
     #}), name='escala'),
->>>>>>> e42a37fde7120131945d656b212baca2626456c8
 
 ]
