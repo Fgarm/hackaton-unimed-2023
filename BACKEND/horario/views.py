a787a90fec95c17ab1b5901f5876784d49848d2f
Fgarm <guilhermear.maturana@outlook.com>
2023-10-04 21:29:04 -0300

    stable but needs things

--- BACKEND/horario/views.py
+++ BACKEND/horario/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
 
-# Create your views here.
+from .models import Horario
+from .serializer import HorarioSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from django.http import JsonResponse
+# from datetime import datetime
+# from django.utils.timezone import datetime
+
+
+class HorarioApiView(APIView):
+
+    @api_view(['POST'])
+    def cadastrar_horario(request):
+            pass
+            
