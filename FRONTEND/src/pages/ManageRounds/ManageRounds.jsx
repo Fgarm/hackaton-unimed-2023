6e4ea06727d846aa2e294e0e694c98d0a71c1187
RenanGAS <renansakaoki@gmail.com>
2023-10-05 03:18:29 -0300

    supervisor-rounds: implementa um parser para o json do backend

--- FRONTEND/src/pages/ManageRounds/ManageRounds.jsx
+++ FRONTEND/src/pages/ManageRounds/ManageRounds.jsx
@@ -1,28 +1,50 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import './styles.css';
-import { Calendar, Layout, Space, Button, Divider, List, Typography, Avatar, Col, Row } from 'antd';
+import { Calendar, Badge, Layout, Space, Button, Divider, List, Typography, Avatar, Col, Row } from 'antd';
 import Home from "../../pages/Home/Home";
 import { UserOutlined } from '@ant-design/icons';
 import axios from 'axios';
 
 const { Header, Footer, Sider, Content } = Layout;
 
+const schedule = {
+    "firstMonth": {
+        "2023-08-05": [
+            { hours: '7:00 - 13:00', content: 'Dr. Nome' },
+            { hours: '13:00 - 19:00', content: 'Dr. Nome' },
+            { hours: '19:00 - 7:00', content: 'Dr. Nome' },
+        ],
+        "2023-08-06": [
+            { hours: '7:00 - 13:00', content: 'Dr. Nome' },
+        ]
+    },
+    "secondMonth": {
+        "2023-09-05": [
+            { hours: '7:00 - 13:00', content: 'Dr. Nome' },
+            { hours: '13:00 - 19:00', content: 'Dr. Nome' },
+            { hours: '19:00 - 7:00', content: 'Dr. Nome' },
+        ],
+        "2023-09-06": [
+            { hours: '7:00 - 13:00', content: 'Dr. Nome' },
+        ]
+    },
+    "thridMonth": {
+        "2023-10-05": [
+            { hours: '7:00 - 13:00', content: 'Dr. Nome' },
+            { hours: '13:00 - 19:00', content: 'Dr. Nome' },
+            { hours: '19:00 - 7:00', content: 'Dr. Nome' },
+        ],
+        "2023-10-06": [
+            { hours: '7:00 - 13:00', content: 'Dr. Nome' },
+        ]
+    }
+}
 
 const scaleStyle = {
-    // textAlign: 'center',
-    // height: 64,
-    // paddingInline: 50,
-    // lineHeight: '64px',
     position: 'relative',
 };
 
 const navigateButtonsScaleStyle = {
-    // textAlign: 'center',
-    // // minHeight: 120,
-    // // lineHeight: '120px',
-    // top: '100px',
-    // color: '#fff',
-    // backgroundColor: '#108ee9',
     paddingTop: '20px',
     position: 'relative',
 };
@@ -46,22 +68,161 @@ const data = [
     },
 ];
 
+const getMonthData = (value) => {
+    if (value.month() === 8) {
+        return 1394;
+    }
+};
+
 export default function ManageRoundsPage() {
-    const onPanelChange = (value, mode) => {
-        console.log(value.format('YYYY-MM-DD'), mode);
+    // const onPanelChange = (value, mode) => {
+    //     console.log(value.format('YYYY-MM-DD'), mode);
+    // };
+
+    // const createSchedule = () => {
+    //     axios.post("http://localhost:6667/doctors/").then((res) => {
+    //         console.log(res.data);
+    //     });
+    // }
+
+    useEffect(() => {
+        if (run) {
+            getPlantaos();
+        }
+        setRun(false);
+    });
+
+    const [parsedPlantoes, setParsedPlantoes] = useState({});
+    const [run, setRun] = useState(true);
+
+    const getListData = (value) => {
+        let listData;
+
+        for (var dateKey in parsedPlantoes) {
+            if (value.date().toString() == dateKey.toString()) {
+                listData = parsedPlantoes[dateKey.toString()];
+                return listData;
+            }
+        }
+        // switch (value.date()) {
+        //     case 8:
+        //         listData = [
+        //             {
+        //                 type: "warning",
+        //                 content: "This is warning event.",
+        //             },
+        //             {
+        //                 type: "success",
+        //                 content: "This is usual event.",
+        //             },
+        //         ];
+        //         break;
+        //     default:
+        // }
+
+        return listData || [];
     };
 
-    const createSchedule = () => {
-        axios.post("http://localhost:6667/doctors/").then((res) => {
-            console.log(res.data);
-        });
+    function getPlantaos() {
+        // await Promise.all(
+        //     axios.get().then((res) => {
+        //console.log(res.data);
+        var listPlantaos = [{ "data": "2023-04-29", "inicio": 780, "final": 1900, "valido": true, "funcionario": 1, "escala": "51d22dad-4717-4edd-8ebe-2818b68cb541" }, { "data": "2023-04-29", "inicio": 780, "final": 1900, "valido": true, "funcionario": 1, "escala": "51d22dad-4717-4edd-8ebe-2818b68cb541" }, { "data": "2023-04-30", "inicio": 780, "final": 1900, "valido": true, "funcionario": 2, "escala": "51d22dad-4717-4edd-8ebe-2818b68cb541" }, { "data": "2023-04-31", "inicio": 780, "final": 1900, "valido": true, "funcionario": 2, "escala": "51d22dad-4717-4edd-8ebe-2818b68cb541" }];
+
+        const transformedPlantaos = {  // tendeu
+        }
+
+        // for (var key in transformedPlantaos) {
+        //     console.log(key);
+        // }
+
+        for (let index = 0; index < listPlantaos.length; index++) {
+            const plantao = listPlantaos[index];
+            // data: "2023-01-01",
+            // inicio: 0
+            // final: 190000
+            // funcionario: 1
+            // escala: "id_escala_UUID"
+
+            const date = new Date(plantao.data);
+
+            const hours_init = Math.floor(plantao.inicio / 3600)
+            const minutes_init = Math.floor((plantao.inicio % 3600) / 60)
+
+            const init_plantao = `${hours_init}:${minutes_init}`;
+
+            const hours_final = Math.floor(plantao.final / 3600)
+            const minutes_final = Math.floor((plantao.final % 3600) / 60)
+
+            const final_plantao = `${hours_final}:${minutes_final}`;
+
+            if (transformedPlantaos.hasOwnProperty((date.getDate() + 1).toString())) {
+                transformedPlantaos[(date.getDate() + 1).toString()].push({ type: `${init_plantao} - ${final_plantao}`, content: `ID medico: ${plantao.funcionario}` });
+
+            } else {
+                transformedPlantaos[(date.getDate() + 1).toString()] = [];
+                transformedPlantaos[(date.getDate() + 1).toString()].push({ type: `${init_plantao} - ${final_plantao}`, content: `ID medico: ${plantao.funcionario}` });
+            }
+            //}
+        }
+        console.log(transformedPlantaos);
+        setParsedPlantoes(transformedPlantaos);
+        // })
+        //)
+    };
+
+    const [mes, setMes] = useState("Outubro");
+
+    const onPanelChange = (value) => {
+        console.log(numeroMes[value.$M] + 1);
+        setMes(numeroMes[value.$M + 1]);
+    };
+
+    function onSelect(date, info) {
+        console.log(date.$D);
     }
 
-    const getDoctors = () => {
-        axios.post("http://localhost:6667/doctors/").then((res) => {
-            console.log(res.data);
-        });
-    }
+    const monthCellRender = (value) => {
+        const num = getMonthData(value);
+        return num ? (
+            <div className="notes-month">
+                <section>{num}</section>
+                <span>Backlog number</span>
+            </div>
+        ) : null;
+    };
+
+    const dateCellRender = (value) => {
+        const listData = getListData(value);
+        return (
+            <ul className="events">
+                {listData.map((item) => (
+                    <li key={item.content}>
+                        <Badge status={item.type} text={item.type + " - " + item.content} />
+                    </li>
+                ))}
+            </ul>
+
+            // Tenho que fazer o display de todos plantoes de um dia de uma vez
+            // Teria que arrumar os dados para:
+            // dia x:
+            // plantao 1
+            // plantao 2
+            // dia y:
+            // plantao 1
+            // plantao 2
+            // Mas tenho:
+            // dia x, plantao 1
+            // dia y, plantao 1
+            // dia x, plantao 2
+        );
+    };
+
+    const cellRender = (current, info) => {
+        if (info.type === "date") return dateCellRender(current);
+        if (info.type === "month") return monthCellRender(current);
+        return info.originNode;
+    };
 
     return (
         <>
@@ -82,7 +243,8 @@ export default function ManageRoundsPage() {
                 size={[0, 48]}
             >
                 <h1>Escala Plantão</h1>
-                <Calendar style={scaleStyle} onPanelChange={onPanelChange} />
+                <Calendar style={scaleStyle} onPanelChange={onPanelChange} onSelect={onSelect}
+                    cellRender={cellRender} />
                 <Space wrap style={navigateButtonsScaleStyle}>
                     <Button>Mês anterior</Button>
                     <Button>Próxima rodada</Button>
