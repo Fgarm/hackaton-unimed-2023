47453a6524ae685a5daa0e9dc5d79bcfa525032e
diogorodriguees <diogorodrigueslife@gmail.com>
2023-10-04 21:23:37 -0300

    :sparkles: save

---
+++ FRONTEND/src/components/SideBar/index.jsx
@@ -0,0 +1,14 @@
+import "./style.css";
+
+import React from "react";
+
+export default function SideBar() {
+    return (
+        <div className="side-bar">
+            <div className="options">
+                <a>Meus Horarios</a>
+                <a>Entrar em uma escala</a>
+            </div>
+        </div>
+    );
+}
--- FRONTEND/src/pages/Home/Home.jsx
+++ FRONTEND/src/pages/Home/Home.jsx
@@ -1,16 +1,55 @@
 import { useNavigate } from "react-router-dom";
 
+import { Calendar } from "antd";
+import { useEffect, useState } from "react";
 import "./styles.css";
+import SideBar from "../../components/SideBar";
 
 export default function Home() {
     const navigate = useNavigate();
+    const [mes, setMes] = useState("Outubro");
+    const numeroMes = {
+        1: "Janeiro",
+        2: "Fevereiro",
+        3: "Março",
+        4: "Abril",
+        5: "Maio",
+        6: "Junho",
+        7: "Julho",
+        8: "Agosto",
+        9: "Setembro",
+        10: "Outubro",
+        11: "Novembro",
+        12: "Dezembro",
+    };
+    const onPanelChange = (value) => {
+        setMes(numeroMes[value.$M]);
+    };
 
+    useEffect(() => {}, []);
     return (
         <div className="home">
-            <div className="side-bar">
-                <div className="options">
-                    <a>Meus Horarios</a>
-                    <a>Entrar em uma escala</a>
+            <SideBar />
+            <div className="calendar">
+                <h1>Agenda do Mês de {mes}</h1>
+                <Calendar onPanelChange={onPanelChange} />
+
+                <div className="horarios">
+                    <h2>8 de Setembro</h2>
+                    <div className="horarios-dia">
+                        <div className="hora">
+                            <p>07:00 as 13:00</p>
+                            <button>Avisar Ausencia</button>
+                        </div>
+                        <div className="hora">
+                            <p>07:00 as 13:00</p>
+                            <button>Avisar Ausencia</button>
+                        </div>
+                        <div className="hora">
+                            <p>07:00 as 13:00</p>
+                            <button>Avisar Ausencia</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
